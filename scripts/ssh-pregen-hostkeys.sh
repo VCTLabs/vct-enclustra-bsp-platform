81d6d1b3ccdfe29a25f1f50b5801e9c09781fc88
Stephen L Arnold <sarnold@vctlabs.com>
2026-04-09 16:44:39 -0700

    chg: usr: add helper script to generate ssh host keys, add to tox
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

---
+++ scripts/ssh-pregen-hostkeys.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# generate SSH host keys for yocto image, uses custom recipe to install
+# run as needed for dev builds; should always run prior to prod builds
+
+TOP_DIR=${1:-""}
+DEF_PATH=./layers/meta-user-aa1
+DEBUG=${2:-"1"}
+
+
+! [[ -z "${TOP_DIR}" ]]  || TOP_DIR="${DEF_PATH}"
+
+ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -N ""
+ssh-keygen -t ed25519 -f ssh_host_ed25519_key -N ""
+
+# dropbearkey -t rsa -s 4096 -f dropbear_rsa_host_key
+# dropbearkey -t ed25519 -f dropbear_ed25519_host_key
+
+! [[ "${DEBUG}" = "1" ]]  || echo "Running with TOP_DIR:  ${TOP_DIR}"
+
+mv -v ssh_host_*_key* "${TOP_DIR}/recipes-connectivity/ssh-pregen-hostkeys/ssh-pregen-hostkeys"
+# mv -v dropbear_*_host_key "${TOP_DIR}/recipes-connectivity/ssh-pregen-hostkeys/ssh-pregen-hostkeys"
